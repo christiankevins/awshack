5a62fd2e6cd369329a3551c44e524d57f33a1283
christiankevins <christian.kevin.04@gmail.com>
2024-03-22 10:36:08 -0400

    First

---
+++ src/Routes.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useRoutes } from "react-router-dom";
+import Home from "pages/Home";
+import NotFound from "pages/NotFound";
+import Signup from "pages/Signup";
+import Home1 from "pages/Home1";
+import Events from "pages/Events";
+import SingleEvent from "pages/SingleEvent";
+import SearchResults from "pages/SearchResults";
+
+const ProjectRoutes = () => {
+  let element = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "*", element: <NotFound /> },
+    {
+      path: "signup",
+      element: <Signup />,
+    },
+    {
+      path: "home1",
+      element: <Home1 />,
+    },
+    {
+      path: "events",
+      element: <Events />,
+    },
+    {
+      path: "singleevent",
+      element: <SingleEvent />,
+    },
+    {
+      path: "searchresults",
+      element: <SearchResults />,
+    },
+  ]);
+
+  return element;
+};
+
+export default ProjectRoutes;
---
+++ src/components/index.jsx
@@ -0,0 +1,8 @@
+import { Text } from "./Text";
+import { Heading } from "./Heading";
+import { Img } from "./Img";
+import { ReactTable } from "./ReactTable";
+import { Input } from "./Input";
+import { Button } from "./Button";
+import { GoogleMap } from "./GoogleMap";
+export { Text, Heading, Img, ReactTable, Input, Button, GoogleMap };
